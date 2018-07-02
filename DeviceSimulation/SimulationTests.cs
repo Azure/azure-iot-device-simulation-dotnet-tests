aa0f3061d5b638167d3e43ec6f2dd39fdb3616b7
hathind-ms <39630244+hathind-ms@users.noreply.github.com>
2018-07-02 15:12:17 -0700

    Adding heartbeat test for Simulation Service

--- DeviceSimulation/SimulationTests.cs
+++ DeviceSimulation/SimulationTests.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using System.Net;
+using Helpers.Http;
+using Xunit;
 
 namespace DeviceSimulation
 {
-    class SimulationTests
+    public class SimulationTests
     {
-        static void SimulationTest(string[] args)
+        private readonly IHttpClient httpClient;
+        private const string DS_ADDRESS = "http://localhost:9003/v1";
+
+        public SimulationTests()
         {
-            Console.WriteLine("Hello World!");
+            this.httpClient = new HttpClient();
+        }
+
+        /// <summary>
+        /// Integration test using a real HTTP instance.
+        /// Test that the service starts normally and returns ok status
+        /// </summary>
+        [Fact]
+        public void Should_Return_OK_Status()
+        {
+            // Act
+            var request = new HttpRequest(DS_ADDRESS + "/status");
+            request.AddHeader("X-Foo", "Bar");
+            var response = this.httpClient.GetAsync(request).Result;
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         }
     }
 }
