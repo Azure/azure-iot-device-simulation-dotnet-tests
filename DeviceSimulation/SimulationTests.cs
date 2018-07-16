1c2779e62a33248cee118f7afc171a51ffa82aa6
Saisruthi Sathyanarayanan <sasathy@microsoft.com>
2018-07-16 13:50:26 -0700

    PR feedback changes

--- DeviceSimulation/SimulationTests.cs
+++ DeviceSimulation/SimulationTests.cs
@@ -18,8 +18,9 @@ namespace DeviceSimulation
         private readonly IHttpClient httpClient;
         private readonly RegistryManager registry;
         private const string DS_ADDRESS = "http://localhost:9003/v1";
-        private const string IOTHUB_CONNECTION_STRING = "HostName=iothub-gi6bx.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=ncS4YgSWTp85ESqCpmkzN99WcWlRl0stMJTu3WfrR10=";
-        private const string PATCH_SIMULATION = "http://localhost:9003/v1/simulations/1";
+        private const string IOTHUB_CONNECTION_STRING = "HostName=SaiDeviceSimulation.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=+dqkraJnTFZjEylsZLRfJCh3Jo8DhOpQ6NOoCAlSWCE=";
+        private const string SIMULATION_URL = "http://localhost:9003/v1/simulations/1";
+        private const int WAIT_TIME = 15000;
         public SimulationTests()
         {
             this.httpClient = new HttpClient();
@@ -44,15 +45,35 @@ namespace DeviceSimulation
         [Fact, Trait("Type", "IntegrationTest")]
         public void Should_Return_Currently_Running_Simulation()
         {
-            var request = new HttpRequest(DS_ADDRESS + "/simulations/1");
-            var response = this.httpClient.GetAsync(request).Result;
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            //Arrange
+            this.Should_Delete_Existing_Simulation();
+            var simulation = JObject.Parse(@"{   
+                'Enabled': true, 
+                'DeviceModels': [   
+                    {   
+                        'Id': 'truck-01', 
+                        'Count': 5 
+                    } 
+                ]
+            }");
+            var createSimulationRequest = new HttpRequest(DS_ADDRESS + "/simulations");
+            createSimulationRequest.SetContent(simulation);
+            var createSimulationResponse = this.httpClient.PostAsync(createSimulationRequest).Result;
+            Assert.Equal(HttpStatusCode.OK, createSimulationResponse.StatusCode);
+
+            //Act
+            var getCurrentSimulationRequest = new HttpRequest(DS_ADDRESS + "/simulations/1");
+            var getCurrentSimulationResponse = this.httpClient.GetAsync(getCurrentSimulationRequest).Result;
+
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, getCurrentSimulationResponse.StatusCode);
+
         }
 
         [Fact, Trait("Type", "IntegrationTest")]
         public void Should_Create_Default_Simulation()
         {
-            //delete the simulation that is already running
+            //Arrange
             this.Should_Delete_Existing_Simulation();
             var simulation = JObject.Parse(@"{   
                 'Enabled': true, 
@@ -63,17 +84,23 @@ namespace DeviceSimulation
                     } 
                 ]
             }");
+
+            //Act
             var request = new HttpRequest(DS_ADDRESS + "/simulations");
-            request.AddHeader("Content-Type", "application/json");
             request.SetContent(simulation);
             var response = this.httpClient.PostAsync(request).Result;
+
+            //Assert
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            var getCurrentSimulationRequest = new HttpRequest(DS_ADDRESS + "/simulations/1");
+            var getCurrentSimulationResponse = this.httpClient.GetAsync(getCurrentSimulationRequest).Result;
+            Assert.Equal(HttpStatusCode.OK, getCurrentSimulationResponse.StatusCode);
         }
 
         [Fact, Trait("Type", "IntegrationTest")]
         public async void Should_Create_SimulatedDevice()
         {
-            //delete the simulation that is already running
+            //Arrange
             this.Should_Delete_Existing_Simulation();
             var simulation = JObject.Parse(@"{   
                 'Enabled': true, 
@@ -84,22 +111,24 @@ namespace DeviceSimulation
                     } 
                 ]
             }");
+
+            //Act
             var request = new HttpRequest(DS_ADDRESS + "/simulations");
-            request.AddHeader("Content-Type", "application/json");
             request.SetContent(simulation);
             var response = this.httpClient.PostAsync(request).Result;
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
 
-            Thread.Sleep(15000);
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Thread.Sleep(WAIT_TIME);
             RegistryManager registry = RegistryManager.CreateFromConnectionString(IOTHUB_CONNECTION_STRING);
-            Twin dev1 = await registry.GetTwinAsync("truck-01.0");
-            Assert.True(dev1.Tags["IsSimulated"] == 'Y');
+            Twin deviceTwin = await registry.GetTwinAsync("truck-01.0");
+            Assert.True(deviceTwin != null && deviceTwin.Tags["IsSimulated"] == 'Y');
         }
 
         [Fact, Trait("Type", "IntegrationTest")]
         public async void Should_Upgrade_Firmware_On_SimulatedDevice()
         {
-            //delete the simulation that is already running
+            //Arrange
             this.Should_Delete_Existing_Simulation();
             var simulation = JObject.Parse(@"{   
                 'Enabled': true, 
@@ -111,24 +140,26 @@ namespace DeviceSimulation
                 ]
             }");
             var request = new HttpRequest(DS_ADDRESS + "/simulations");
-            request.AddHeader("Content-Type", "application/json");
             request.SetContent(simulation);
             var response = this.httpClient.PostAsync(request).Result;
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
 
-            Thread.Sleep(15000);
+            Thread.Sleep(WAIT_TIME);
 
+            //Act
             ServiceClient serviceClient = ServiceClient.CreateFromConnectionString(IOTHUB_CONNECTION_STRING);
-            Task<CloudToDeviceMethodResult> directResponseFuture = serviceClient.InvokeDeviceMethodAsync("truck-01.0",
+            Task<CloudToDeviceMethodResult> firmwareUpgradeMethodResult = serviceClient.InvokeDeviceMethodAsync("truck-01.0",
                 new CloudToDeviceMethod("FirmwareUpdate"));
-            CloudToDeviceMethodResult response1 = await directResponseFuture.ConfigureAwait(false);
-            Assert.Equal(200, response1.Status);
+            CloudToDeviceMethodResult firmwareUpdateResponse = await firmwareUpgradeMethodResult.ConfigureAwait(false);
+
+            //Assert
+            Assert.Equal(200, firmwareUpdateResponse.Status);
         }
 
         [Fact, Trait("Type", "IntegrationTest")]
         public async void Should_Reboot_SimulatedDevice()
         {
-            //delete the simulation that is already running
+            //Arrange
             this.Should_Delete_Existing_Simulation();
             var simulation = JObject.Parse(@"{   
                 'Enabled': true, 
@@ -140,48 +171,67 @@ namespace DeviceSimulation
                 ]
             }");
             var request = new HttpRequest(DS_ADDRESS + "/simulations");
-            request.AddHeader("Content-Type", "application/json");
             request.SetContent(simulation);
             var response = this.httpClient.PostAsync(request).Result;
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
 
-            Thread.Sleep(15000);
+            Thread.Sleep(WAIT_TIME);
 
+            //Act
             ServiceClient serviceClient = ServiceClient.CreateFromConnectionString(IOTHUB_CONNECTION_STRING);
-            Task<CloudToDeviceMethodResult> directResponseFuture = serviceClient.InvokeDeviceMethodAsync("chiller-01.0",
+            Task<CloudToDeviceMethodResult> rebootMethodResult = serviceClient.InvokeDeviceMethodAsync("chiller-01.0",
                 new CloudToDeviceMethod("Reboot"));
-            CloudToDeviceMethodResult response1 = await directResponseFuture.ConfigureAwait(false);
+            CloudToDeviceMethodResult rebootResponse = await rebootMethodResult.ConfigureAwait(false);
 
-            Assert.Equal(200, response1.Status);
+            //Assert
+            Assert.Equal(200, rebootResponse.Status);
         }
 
         [Fact, Trait("Type", "IntegrationTest")]
         public void Should_Start_Given_Simulation()
         {
+            //Arrange
+            this.Should_Delete_Existing_Simulation();
+            var simulation = JObject.Parse(@"{   
+                'Enabled': true, 
+                'DeviceModels': [   
+                    {   
+                        'Id': 'truck-01', 
+                        'Count': 5 
+                    } 
+                ]
+            }");
+            var createSimulationRequest = new HttpRequest(DS_ADDRESS + "/simulations");
+            createSimulationRequest.SetContent(simulation);
+            var createSimulationResponse = this.httpClient.PostAsync(createSimulationRequest).Result;
+            Assert.Equal(HttpStatusCode.OK, createSimulationResponse.StatusCode);
 
-            var request1 = new HttpRequest(DS_ADDRESS + "/simulations/1");
-            var response1 = this.httpClient.GetAsync(request1).Result;
-            Assert.Equal(HttpStatusCode.OK, response1.StatusCode);
-            JObject JsonResponse = JObject.Parse(response1.Content);
+            var currentSimulationRequest = new HttpRequest(DS_ADDRESS + "/simulations/1");
+            var currentSimulationResponse = this.httpClient.GetAsync(currentSimulationRequest).Result;
+            Assert.Equal(HttpStatusCode.OK, currentSimulationResponse.StatusCode);
+            JObject JsonResponse = JObject.Parse(currentSimulationResponse.Content);
 
             string ETag = (string)JsonResponse["ETag"];
 
-            var simulation2 = "{" + $"'ETag': '{ETag}' ,'Enabled': true" + "}";
-            var byteArray = Encoding.ASCII.GetBytes(simulation2);
+            var simulationContent = "{" + $"'ETag': '{ETag}' ,'Enabled': true" + "}";
+            var simulationContentByteArray = Encoding.ASCII.GetBytes(simulationContent);
 
-            var request2 = (HttpWebRequest)WebRequest.Create("http://localhost:9003/v1/simulations/1");
-            request2.Method = "PATCH";
-            request2.ContentLength = byteArray.Length;
-            request2.ContentType = "application/json";
-            Stream dataStream = request2.GetRequestStream();
-            dataStream.Write(byteArray, 0, byteArray.Length);
+            //Act
+            var startSimulationRequest = (HttpWebRequest)WebRequest.Create("http://localhost:9003/v1/simulations/1");
+            startSimulationRequest.Method = "PATCH";
+            startSimulationRequest.ContentLength = simulationContentByteArray.Length;
+            startSimulationRequest.ContentType = "application/json";
+            Stream dataStream = startSimulationRequest.GetRequestStream();
+            dataStream.Write(simulationContentByteArray, 0, simulationContentByteArray.Length);
             dataStream.Close();
-            var response2 = (HttpWebResponse)request2.GetResponse();
-            Assert.Equal(HttpStatusCode.OK, response2.StatusCode);
+            var startSimulationResponse = (HttpWebResponse)startSimulationRequest.GetResponse();
 
-            var request = new HttpRequest(DS_ADDRESS + "/simulations/1");
-            var response = this.httpClient.GetAsync(request).Result;
-            JObject jsonResponse = JObject.Parse(response.Content);
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, startSimulationResponse.StatusCode);
+
+            var verificationRequest = new HttpRequest(DS_ADDRESS + "/simulations/1");
+            var verificationResponse = this.httpClient.GetAsync(verificationRequest).Result;
+            JObject jsonResponse = JObject.Parse(verificationResponse.Content);
 
             Assert.True((bool)jsonResponse["Enabled"]);
         }
@@ -189,41 +239,66 @@ namespace DeviceSimulation
         [Fact, Trait("Type", "IntegrationTest")]
         public void Should_Stop_Given_Simulation()
         {
-            var request1 = new HttpRequest(DS_ADDRESS + "/simulations/1");
-            var response1 = this.httpClient.GetAsync(request1).Result;
-            Assert.Equal(HttpStatusCode.OK, response1.StatusCode);
-            JObject JsonResponse = JObject.Parse(response1.Content);
+            //Arrange
+            this.Should_Delete_Existing_Simulation();
+            var simulation = JObject.Parse(@"{   
+                'Enabled': true, 
+                'DeviceModels': [   
+                    {   
+                        'Id': 'truck-01', 
+                        'Count': 5 
+                    } 
+                ]
+            }");
+            var createSimulationRequest = new HttpRequest(DS_ADDRESS + "/simulations");
+            createSimulationRequest.SetContent(simulation);
+            var createSimulationResponse = this.httpClient.PostAsync(createSimulationRequest).Result;
+            Assert.Equal(HttpStatusCode.OK, createSimulationResponse.StatusCode);
+            var currentSimulationRequest = new HttpRequest(DS_ADDRESS + "/simulations/1");
+            var currentSimulationResponse = this.httpClient.GetAsync(currentSimulationRequest).Result;
+            Assert.Equal(HttpStatusCode.OK, currentSimulationResponse.StatusCode);
+            JObject JsonResponse = JObject.Parse(currentSimulationResponse.Content);
 
             string ETag = (string)JsonResponse["ETag"];
 
-            var simulation2 = "{" + $"'ETag': '{ETag}' ,'Enabled': false" + "}";
-            var byteArray = Encoding.ASCII.GetBytes(simulation2);
+            var simulationContent = "{" + $"'ETag': '{ETag}' ,'Enabled': false" + "}";
+            var simulationContentByteArray = Encoding.ASCII.GetBytes(simulationContent);
 
-            var request2 = (HttpWebRequest)WebRequest.Create(PATCH_SIMULATION);
-            request2.Method = "PATCH";
-            request2.ContentLength = byteArray.Length;
-            request2.ContentType = "application/json";
-            Stream dataStream = request2.GetRequestStream();
-            dataStream.Write(byteArray, 0, byteArray.Length);
+            //Act
+            var startSimulationRequest = (HttpWebRequest)WebRequest.Create(SIMULATION_URL);
+            startSimulationRequest.Method = "PATCH";
+            startSimulationRequest.ContentLength = simulationContentByteArray.Length;
+            startSimulationRequest.ContentType = "application/json";
+            Stream dataStream = startSimulationRequest.GetRequestStream();
+            dataStream.Write(simulationContentByteArray, 0, simulationContentByteArray.Length);
             dataStream.Close();
-            var response2 = (HttpWebResponse)request2.GetResponse();
-            Assert.Equal(HttpStatusCode.OK, response2.StatusCode);
+            var startSimulationResponse = (HttpWebResponse)startSimulationRequest.GetResponse();
 
-            var request = new HttpRequest(DS_ADDRESS + "/simulations/1");
-            var response = this.httpClient.GetAsync(request).Result;
-            JObject jsonResponse = JObject.Parse(response.Content);
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, startSimulationResponse.StatusCode);
+
+            var verificationRequest = new HttpRequest(DS_ADDRESS + "/simulations/1");
+            var verificationResponse = this.httpClient.GetAsync(verificationRequest).Result;
+            JObject jsonResponse = JObject.Parse(verificationResponse.Content);
 
             Assert.False((bool)jsonResponse["Enabled"]);
         }
 
         private void Should_Delete_Existing_Simulation()
         {
-            var request = new HttpRequest(DS_ADDRESS + "/simulations/1");
-            var response = this.httpClient.DeleteAsync(request).Result;
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            var runningSimulationRequest = new HttpRequest(DS_ADDRESS + "/simulations/1");
+            var runningSimulationResponse = this.httpClient.GetAsync(runningSimulationRequest).Result;
+            if(runningSimulationResponse.StatusCode == HttpStatusCode.OK)
+            {
+                var deleteCurrentSimulationRequest = new HttpRequest(DS_ADDRESS + "/simulations/1");
+                var deleteCurrentSimulationResponse = this.httpClient.DeleteAsync(deleteCurrentSimulationRequest).Result;
+                Assert.Equal(HttpStatusCode.OK, deleteCurrentSimulationResponse.StatusCode);
+                
+                var getCurrentSimulationRequest = new HttpRequest(DS_ADDRESS + "/simulations/1");
+                var getCurrentSimulationResponse = this.httpClient.GetAsync(getCurrentSimulationRequest).Result;
+                Assert.Equal(HttpStatusCode.NotFound, getCurrentSimulationResponse.StatusCode);
+            }
+            
         }
-
-
-
     }
 }
