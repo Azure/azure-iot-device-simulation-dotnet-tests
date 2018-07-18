7689d0a202e4f0d0fda5ae22f3f060dd525b7062
Saisruthi Sathyanarayanan <sasathy@microsoft.com>
2018-07-18 10:39:05 -0700

    Changing localhost to 127

--- Helper/Constants.cs
+++ Helper/Constants.cs
@@ -3,8 +3,8 @@ namespace Helpers
     public class Constants
     {
         // Endpoints
-        public const string DS_ADDRESS = "http://localhost:9003/v1";
-        public const string SIMULATION_URL = "http://localhost:9003/v1/simulations/1";
+        public const string DS_ADDRESS = "http://127.0.0.1:9003/v1";
+        public const string SIMULATION_URL = "http://127.0.0.1:9003/v1/simulations/1";
         public const int WAIT_TIME = 20000;
     }
 }
