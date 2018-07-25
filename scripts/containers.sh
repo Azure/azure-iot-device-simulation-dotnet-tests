69f408343dbd3d8e079a1481fb76b681f52e3401
hathind-ms <39630244+hathind-ms@users.noreply.github.com>
2018-07-25 11:11:44 -0700

    Updating env list

--- scripts/containers.sh
+++ scripts/containers.sh
@@ -7,19 +7,13 @@ APP_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )/"
 cd $APP_HOME
 
 start() {
-    ./scripts/pcsconfig.sh start
     ./scripts/storageadapter.sh start
     ./scripts/devicesimulation.sh start
-    ./scripts/telemetry.sh start
-    ./scripts/iothubmanager.sh start
 }
 
 stop() {
-    ./scripts/pcsconfig.sh stop
     ./scripts/storageadapter.sh stop
     ./scripts/devicesimulation.sh stop
-    ./scripts/telemetry.sh stop
-    ./scripts/iothubmanager.sh stop
 }
 
 if [[ "$1" == "start" ]]; then
