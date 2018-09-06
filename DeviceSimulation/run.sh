bc8fd248be77ac5f5689821869cbe1333316f127
hathind-ms <39630244+hathind-ms@users.noreply.github.com>
2018-09-05 18:06:23 -0700

    wip

--- DeviceSimulation/run.sh
+++ DeviceSimulation/run.sh
@@ -39,7 +39,7 @@ run_tests() {
 
     header2 "$TEST_SUITE - Running tests..."
     cd $TEST_HOME
-    find . -name *.csproj p:ParallelizeTestCollections=false | xargs dotnet test --configuration Release
+    find . -name *.csproj -p:ParallelizeTestCollections=false | xargs dotnet test --configuration Release
 }
 
 #Script options  
