dd2da88e7dcc638d56fe9de5619de3ab7e27a972
hathind-ms <39630244+hathind-ms@users.noreply.github.com>
2018-09-05 18:01:58 -0700

    disable parallel test

--- DeviceSimulation/run.sh
+++ DeviceSimulation/run.sh
@@ -39,7 +39,7 @@ run_tests() {
 
     header2 "$TEST_SUITE - Running tests..."
     cd $TEST_HOME
-    find . -name *.csproj | xargs dotnet test --configuration Release
+    find . -name *.csproj | xargs dotnet test --configuration Release -parallel none
 }
 
 #Script options  
